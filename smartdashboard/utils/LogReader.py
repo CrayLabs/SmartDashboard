a28a527d9f44a99b6e7ecbd22fbed0a371b57512
Alyssa Cote <cote@cray.com>
2023-10-16 09:03:54 -0700

    added doc string

--- smartdashboard/utils/LogReader.py
+++ smartdashboard/utils/LogReader.py
@@ -1,4 +1,11 @@
 def get_logs(file: str) -> str:
+    """Get the logs of an entity
+
+    :param file: Log file path of the entity
+    :type file: str
+    :return: Logs o the entity
+    :rtype: str
+    """
     try:
         with open(file, "r", encoding="utf-8") as log_file:
             return log_file.read()
