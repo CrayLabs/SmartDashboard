49775495537d58eeaba81e40f3b4784e51c8a06b
Alyssa Cote <cote@cray.com>
2024-01-24 10:10:21 -0800

    revert help page changes

--- smartdashboard/pages/3_Help.py
+++ smartdashboard/pages/3_Help.py
@@ -38,44 +38,34 @@ local_css(str(curr_path / "static/style.css"))
 
 st.header("Welcome to the SmartSim Dashboard Help Page")
 st.write("")
-st.markdown(
-    """We're here to guide you through the features and functionalities
+st.markdown("""We're here to guide you through the features and functionalities
       of the SmartSim Dashboard,
     designed to enhance your experience with simulation
     experiments. This guide will help
     you navigate through the dashboard, understand its capabilities,
-    and make the most of its powerful features."""
-)
+    and make the most of its powerful features.""")
 st.write("")
 
 with st.expander(label="Experiment Overview"):
-    st.markdown(
-        """Discover comprehensive insights about your experiment's
+    st.markdown("""Discover comprehensive insights about your experiment's
         entities within the Experiment Overview section.
         Get access to configuration details, comprehensive logs,
         real-time statuses, and relevant information regarding any
         colocated databases, if applicable.
-        """
-    )
-    st.markdown(
-        """
+        """)
+    st.markdown("""
         To access detailed information about experiments, models, orchestrators,
         and ensembles, please refer to their respective documentation pages below:
         - [Experiments](https://www.craylabs.org/docs/experiment.html#)
         - [Models](https://www.craylabs.org/docs/experiment.html#model)
         - [Orchestrators](https://www.craylabs.org/docs/orchestrator.html)
-        - [Ensembles](https://www.craylabs.org/docs/experiment.html#ensemble)"""
-    )
+        - [Ensembles](https://www.craylabs.org/docs/experiment.html#ensemble)""")
 
 st.write("")
 
 with st.expander(label="Support"):
-    st.markdown(
-        """Should you encounter any issues or require assistance while
-        using the SmartSim Dashboard, we're here to help!"""
-    )
-    st.markdown(
-        """The complete SmartSim documentation can be found
+    st.markdown("""Should you encounter any issues or require assistance while
+        using the SmartSim Dashboard, we're here to help!""")
+    st.markdown("""The complete SmartSim documentation can be found
         [here](https://www.craylabs.org/docs/overview.html).
-        You can also contact us at craylabs@hpe.com."""
-    )
+        You can also contact us at craylabs@hpe.com.""")
